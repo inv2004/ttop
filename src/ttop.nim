2e9c3c9f809974bffe81dba62ecf08149bded663
inv2004 <inv2004@gmail.com>
2022-12-23 00:57:00 +0300

    cligen removed

--- src/ttop.nim
+++ src/ttop.nim
@@ -2,54 +2,45 @@ import ttop/tui
 import ttop/blog
 import ttop/onoff
 
-import tables
-
 import os
 
-proc isSave(): bool =
-  if paramCount() >= 1 and paramStr(1) in ["-s", "--save"]:
-    true
-  else:
-    false
+const Help = """
+Usage:
+  run [optional-param]
+Options:
+  -h, --help     print this cligen-erated help
+  -s, --save     save snapshot
+  --on           enable system.timer collector
+  --off          disable collector
+"""
 
-proc isEnable(): bool =
-  if paramCount() >= 1 and paramStr(1) in ["-on", "--on"]:
-    true
-  else:
-    false
-
-proc isDisable(): bool =
-  if paramCount() >= 1 and paramStr(1) in ["-off", "--off"]:
-    true
-  else:
-    false
-
-proc run(save = false, on = false, off = false) =
+proc main() =
   try:
-    if on: onoff(true)
-    elif off: onoff(false)
-    elif save: save()
-    else: tui()
+    case paramCount():
+    of 0:
+      tui()
+    of 1:
+      case paramStr(1)
+      of "-h", "--help":
+        echo Help
+      of "-s", "--save":
+        save()
+      of "--on":
+        onoff(true)
+      of "--off":
+        onoff(false)
+      else:
+        echo Help
+        quit 1
+    else:
+      echo Help
+      quit 1
   except CatchableError:
     let ex = getCurrentException()
     echo ex.msg
     echo ex.getStackTrace()
     quit 1
 
-const Help = {
-    "save": "save snapshot",
-    "on": "enable system.timer collector",
-    "off": "disable collector"
-  }.toTable
-
-const Short = {
-    "save": 's',
-    "on": '\0',
-    "off": '\0'
-  }.toTable
-
 when isMainModule:
-  import cligen
-
-  dispatch run, help = Help, short = Short
+  main()
 
--- ttop.nimble
+++ ttop.nimble
@@ -15,7 +15,6 @@ requires "nim >= 1.6.4"
 requires "illwill"
 requires "zippy"
 requires "asciigraph"
-requires "cligen"
 
 task static, "build static release":
   exec "nim -d:release --gcc.exe:musl-gcc --gcc.linkerexe:musl-gcc --passL:-static -o:ttop c src/ttop.nim"

9d0ccae61e56d90a5baffb3c10d9048cd43b4b58
inv2004 <inv2004@gmail.com>
2023-01-10 13:55:56 +0300

    onoff: crontab support

--- src/ttop/onoff.nim
+++ src/ttop/onoff.nim
@@ -8,6 +8,8 @@ const timer = "*:0/10:08"
 const unit = "ttop"
 const descr = "ttop service snapshot collector"
 
+const cron = "*/10 * * * * ttop -s"
+
 const options = {poUsePath, poEchoCmd, poStdErrToStdOut}
 
 proc createService(file: string, app: string) =
@@ -61,30 +63,60 @@ proc deleteConfig() =
   echo "delete ", file
   removeFile file
 
-proc cmd(cmd: string, check = false) =
-  let (output, code) = execCmdEx(cmd, options = options)
-  if output.len > 0:
-    echo output
-    if check and output.splitLines()[0] != "inactive":
-      let msg = "Looks like ttop.timer is already running system-wide"
-      raise newException(ValueError, "cmd error: " & msg)
-    else:
-      return
+proc cmd(cmd: string, check = false, input = ""): string =
+  var code: int
+  (result, code) = execCmdEx(cmd, options = options, input = input)
+  if result.len > 0:
+    echo result
+    if check:
+      let line0 = result.splitLines()[0]
+      if line0 == "active":
+        let msg = "Looks like ttop.timer is already running system-wide"
+        raise newException(ValueError, "cmd error: " & msg)
+      if line0.startsWith "no crontab for":
+        return ""
   if code != 0:
-    raise newException(ValueError, "cmd error code")
+    raise newException(ValueError, "cmd error code: " & $code)
+
+proc onOffSystemd(enable: bool) =
+  if enable:
+    discard cmd(&"systemctl check '{unit}.timer'", true)
+    createConfig()
+    discard cmd "systemctl --user daemon-reload"
+    discard cmd &"systemctl --user start '{unit}.timer'"
+    discard cmd "loginctl enable-linger"
+  else:
+    discard cmd &"systemctl --user stop '{unit}.timer'"
+    deleteConfig()
+    discard cmd "systemctl --user daemon-reload"
+    discard cmd "loginctl disable-linger"
+
+proc filter(input: string): string =
+  for l in input.splitLines(true):
+    if "ttop" in l:
+      continue
+    result.add l
+
+proc onOffCron(enable: bool) =
+  let output = cmd("crontab -l", true)
+  var input = filter(output)
+  if enable:
+    input &= cron & "\n"
+    discard cmd("crontab", false, input)
+  else:
+    if input == "":
+      discard cmd "crontab -r"
+    else:
+      discard cmd("crontab", false, input)
+  discard cmd("crontab -l", true)
 
 proc onoff*(enable: bool) =
-  if enable:
-    cmd &"systemctl check '{unit}.timer'", true
-    createConfig()
-    cmd "systemctl --user daemon-reload"
-    cmd &"systemctl --user start '{unit}.timer'"
-    cmd "loginctl enable-linger"
-  else:
-    cmd &"systemctl --user stop '{unit}.timer'"
-    deleteConfig()
-    cmd "systemctl --user daemon-reload"
-    cmd "loginctl disable-linger"
+  try:
+    discard cmd "systemctl is-active --quiet service"
+    onOffSystemd(enable)
+  except CatchableError:
+    echo "systemd failed, trying crontab"
+    onOffCron(enable)
 
 proc createPkgConfig() =
   let pkgBin = "/usr/bin/ttop"
